39179e5d92665a3ca243b8747354d3f23f036ed0
Zer0177 <jamesmehew@yahoo.ca>
2023-12-03 16:14:56 -0500

    Adding the armors back

---
+++ Armor/Hephaestus_Camo/Grenadier/Config.cpp
@@ -0,0 +1,341 @@
+class CfgPatches {
+	class 53rd_Hephaestus_Camo_Armor_Grenaider
+	{
+			units[] = 
+			{
+
+			};
+			weapons[] = 
+			{
+			};
+			requiredVersion = 0.100000;
+			requiredAddons[] = {};
+	};
+};
+#include "..\..\xtdGear.hpp"	
+class cfgWeapons
+{
+	class ItemCore;
+	class UniformItem;
+	class Uniform_Base: ItemCore
+	{
+		class ItemInfo;
+	};
+	class haloinfmar_U_WDL_uniform: Uniform_Base
+	{
+		class ItemInfo;
+	};
+	class 53rd_U_Base_GL_uniform: haloinfmar_U_WDL_uniform
+	{
+		scope=1;
+		scopeArsenal=1;
+		side=1;
+		displayName = "[53rd] GL Base";
+		class ItemInfo: UniformItem
+		{
+			uniformType = "Neopren";
+			uniformModel="-";
+			uniformClass="53rd_GL_base_F";
+			containerClass="Supply100";
+			mass=80;
+		};
+		hiddenSelections[]=
+		{
+			"Camo1",
+			"Camo2",
+			"Camo3"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"53rd_SC_aux\Armor\Hephaestus_Camo\Rifleman\53rd_Rifleman_top_Winter_co.paa",
+			"53rd_SC_aux\Armor\Hephaestus_Camo\Rifleman\53rd_Camo_Pants_Winter_co.paa",
+			"53rd_SC_aux\Armor\Hephaestus_Camo\Grenadier\53rd_Grenadier_Armor_Winter_co.paa"
+		};
+	};
+	class 53rd_Uniform_GL_SN: 53rd_U_Base_GL_uniform
+	{
+		author="53rd Mod Team";
+		scope=2;
+		scopeArsenal=2;
+		side=1;
+		allowedSlots[]={901};
+		displayName="[53rd] Winter Grenadier";
+		picture="\A3\characters_f\data\ui\icon_U_BasicBody_CA.paa";
+		model="\A3\Characters_F\Common\Suitpacks\suitpack_blufor_diver";
+		hiddenSelections[]=
+		{
+			"Camo1",
+			"Camo2",
+			"Camo3"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"53rd_SC_aux\Armor\Hephaestus_Camo\Rifleman\53rd_Rifleman_top_Winter_co.paa",
+			"53rd_SC_aux\Armor\Hephaestus_Camo\Rifleman\53rd_Camo_Pants_Winter_co.paa",
+			"53rd_SC_aux\Armor\Hephaestus_Camo\Grenadier\53rd_Grenadier_Armor_Winter_co.paa"
+		};
+		class ItemInfo: ItemInfo
+		{
+			hiddenSelections[]=
+			{
+				"Camo1",
+				"Camo2",
+				"Camo3"
+			};
+			hiddenSelectionsTextures[]=
+			{
+			"53rd_SC_aux\Armor\Hephaestus_Camo\Rifleman\53rd_Rifleman_top_Winter_co.paa",
+			"53rd_SC_aux\Armor\Hephaestus_Camo\Rifleman\53rd_Camo_Pants_Winter_co.paa",
+			"53rd_SC_aux\Armor\Hephaestus_Camo\Grenadier\53rd_Grenadier_Armor_Winter_co.paa"
+			};
+			uniformModel="-";
+			uniformClass="53rd_GL_Snow_Camo_Test";
+			containerClass="Supply90";
+			mass=80;
+		};
+	};
+		class 53rd_Uniform_GL_WD: 53rd_U_Base_GL_uniform
+	{
+		author="53rd Mod Team";
+		scope=2;
+		scopeArsenal=2;
+		side=1;
+		allowedSlots[]={901};
+		displayName="[53rd] Woodland Grenadier";
+		picture="\A3\characters_f\data\ui\icon_U_BasicBody_CA.paa";
+		model="\A3\Characters_F\Common\Suitpacks\suitpack_blufor_diver";
+		hiddenSelections[]=
+		{
+			"Camo1",
+			"Camo2",
+			"Camo3"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"53rd_SC_aux\Armor\Hephaestus_Camo\Rifleman\53rd_Rifleman_top_Woodland_co.paa",
+			"53rd_SC_aux\Armor\Hephaestus_Camo\Rifleman\53rd_Camo_Pants_Woodland_co.paa",
+			"53rd_SC_aux\Armor\Hephaestus_Camo\Grenadier\53rd_Grenadier_Armor_Woodland_co.paa"
+		};
+		class ItemInfo: ItemInfo
+		{
+			hiddenSelections[]=
+			{
+				"Camo1",
+				"Camo2",
+				"Camo3"
+			};
+			hiddenSelectionsTextures[]=
+			{
+			"53rd_SC_aux\Armor\Hephaestus_Camo\Rifleman\53rd_Rifleman_top_Woodland_co.paa",
+			"53rd_SC_aux\Armor\Hephaestus_Camo\Rifleman\53rd_Camo_Pants_Woodland_co.paa",
+			"53rd_SC_aux\Armor\Hephaestus_Camo\Grenadier\53rd_Grenadier_Armor_Woodland_co.paa"
+			};
+			uniformModel="-";
+			uniformClass="53rd_GL_Woodland_Camo_Test";
+			containerClass="Supply90";
+			mass=80;
+		};
+	};
+		class 53rd_Uniform_GL_UB: 53rd_U_Base_GL_uniform
+	{
+		author="53rd Mod Team";
+		scope=2;
+		scopeArsenal=2;
+		side=1;
+		allowedSlots[]={901};
+		displayName="[53rd] Urban Grenadier";
+		picture="\A3\characters_f\data\ui\icon_U_BasicBody_CA.paa";
+		model="\A3\Characters_F\Common\Suitpacks\suitpack_blufor_diver";
+		hiddenSelections[]=
+		{
+			"Camo1",
+			"Camo2",
+			"Camo3"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"53rd_SC_aux\Armor\Hephaestus_Camo\Rifleman\53rd_Rifleman_top_Urban_co.paa",
+			"53rd_SC_aux\Armor\Hephaestus_Camo\Rifleman\53rd_Camo_Pants_Urban_co.paa",
+			"53rd_SC_aux\Armor\Hephaestus_Camo\Grenadier\53rd_Grenadier_Armor_Urban_co.paa"
+		};
+		class ItemInfo: ItemInfo
+		{
+			hiddenSelections[]=
+			{
+				"Camo1",
+				"Camo2",
+				"Camo3"
+			};
+			hiddenSelectionsTextures[]=
+			{
+			"53rd_SC_aux\Armor\Hephaestus_Camo\Rifleman\53rd_Rifleman_top_Urban_co.paa",
+			"53rd_SC_aux\Armor\Hephaestus_Camo\Rifleman\53rd_Camo_Pants_Urban_co.paa",
+			"53rd_SC_aux\Armor\Hephaestus_Camo\Grenadier\53rd_Grenadier_Armor_Urban_co.paa"
+			};
+			uniformModel="-";
+			uniformClass="53rd_GL_Urban_Camo_Test";
+			containerClass="Supply90";
+			mass=80;
+		};
+	};
+		class 53rd_Uniform_GL_DS: 53rd_U_Base_GL_uniform
+	{
+		author="53rd Mod Team";
+		scope=2;
+		scopeArsenal=2;
+		side=1;
+		allowedSlots[]={901};
+		displayName="[53rd] Desert Grenadier";
+		picture="\A3\characters_f\data\ui\icon_U_BasicBody_CA.paa";
+		model="\A3\Characters_F\Common\Suitpacks\suitpack_blufor_diver";
+		hiddenSelections[]=
+		{
+			"Camo1",
+			"Camo2",
+			"Camo3"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"53rd_SC_aux\Armor\Hephaestus_Camo\Rifleman\53rd_Desert_Camo_Top_co.paa",
+			"53rd_SC_aux\Armor\Hephaestus_Camo\Rifleman\53rd_Camo_Pants_Desert_co.paa",
+			"53rd_SC_aux\Armor\Hephaestus_Camo\Grenadier\53rd_Grenadier_Armor_Desert_co.paa"
+		};
+		class ItemInfo: ItemInfo
+		{
+			hiddenSelections[]=
+			{
+				"Camo1",
+				"Camo2",
+				"Camo3"
+			};
+			hiddenSelectionsTextures[]=
+			{
+			"53rd_SC_aux\Armor\Hephaestus_Camo\Rifleman\53rd_Desert_Camo_Top_co.paa",
+			"53rd_SC_aux\Armor\Hephaestus_Camo\Rifleman\53rd_Camo_Pants_Desert_co.paa",
+			"53rd_SC_aux\Armor\Hephaestus_Camo\Grenadier\53rd_Grenadier_Armor_Desert_co.paa"
+			};
+			uniformModel="-";
+			uniformClass="53rd_GL_Desert_Camo_Test";
+			containerClass="Supply90";
+			mass=80;
+		};
+	};
+};
+class CfgVehicles
+{
+	class B_Soldier_base_F;
+	class haloinf_Marine_Soldier_base_F: B_Soldier_base_F{};
+	class HaloInf_Marine_WDL_F: haloinf_Marine_Soldier_base_F
+	{
+	class UniformItem;
+	};
+	class 53rd_GL_base_F: HaloInf_Marine_WDL_F
+	{
+		displayName="[53rd] Base Uniform";
+		class ItemInfo: UniformItem
+		{
+			uniformType = "Neopren";
+			uniformModel="-";
+			uniformClass="53rd_U_Base_GL_uniform";
+			containerClass="Supply100";
+			mass=80;
+		};
+		hiddenSelections[]=
+		{
+			"Camo1",
+			"Camo2",
+			"Camo3"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"53rd_SC_aux\Armor\Hephaestus_Camo\Rifleman\53rd_Rifleman_top_Winter_co.paa",
+			"53rd_SC_aux\Armor\Hephaestus_Camo\Rifleman\53rd_Camo_Pants_Winter_co.paa",
+			"53rd_SC_aux\Armor\Hephaestus_Camo\Grenadier\53rd_Grenadier_Armor_Winter_co.paa"
+		};
+	};
+	class 53rd_GL_Snow_Camo_Test: 53rd_GL_base_F
+	{
+		scope=2;
+		scopeArsenal=2;
+		side=1;
+		displayName="[53rd] Winter Grenadier";
+		hiddenSelections[]=
+		{
+			"Camo1",
+			"Camo2",
+			"Camo3"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"53rd_SC_aux\Armor\Hephaestus_Camo\Rifleman\53rd_Rifleman_top_Winter_co.paa",
+			"53rd_SC_aux\Armor\Hephaestus_Camo\Rifleman\53rd_Camo_Pants_Winter_co.paa",
+			"53rd_SC_aux\Armor\Hephaestus_Camo\Grenadier\53rd_Grenadier_Armor_Winter_co.paa"
+		};
+		model="\Halo_marine_02\Halo_Marine_02_Uniform_F.p3d";
+		uniformClass="53rd_Uniform_GL_SN";
+	};
+	class 53rd_GL_Woodland_Camo_Test: 53rd_GL_base_F
+	{
+		scope=2;
+		scopeArsenal=2;
+		side=1;
+		displayName="[53rd] Woodland Grenadier";
+		hiddenSelections[]=
+		{
+			"Camo1",
+			"Camo2",
+			"Camo3"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"53rd_SC_aux\Armor\Hephaestus_Camo\Rifleman\53rd_Rifleman_top_Woodland_co.paa",
+			"53rd_SC_aux\Armor\Hephaestus_Camo\Rifleman\53rd_Camo_Pants_Woodland_co.paa",
+			"53rd_SC_aux\Armor\Hephaestus_Camo\Grenadier\53rd_Grenadier_Armor_Woodland_co.paa"
+		};
+		model="\Halo_marine_02\Halo_Marine_02_Uniform_F.p3d";
+		uniformClass="53rd_Uniform_GL_WD";
+	};
+	class 53rd_GL_Urban_Camo_Test: 53rd_GL_base_F
+	{
+		scope=2;
+		scopeArsenal=2;
+		side=1;
+		displayName="[53rd] Urban Grenadier";
+		hiddenSelections[]=
+		{
+			"Camo1",
+			"Camo2",
+			"Camo3"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"53rd_SC_aux\Armor\Hephaestus_Camo\Rifleman\53rd_Rifleman_top_Urban_co.paa",
+			"53rd_SC_aux\Armor\Hephaestus_Camo\Rifleman\53rd_Camo_Pants_Urban_co.paa",
+			"53rd_SC_aux\Armor\Hephaestus_Camo\Grenadier\53rd_Grenadier_Armor_Urban_co.paa"
+		};
+		model="\Halo_marine_02\Halo_Marine_02_Uniform_F.p3d";
+		uniformClass="53rd_Uniform_GL_UB";
+	};
+	class 53rd_GL_Desert_Camo_Test: 53rd_GL_base_F
+	{
+		scope=2;
+		scopeArsenal=2;
+		side=1;
+		displayName="[53rd] Desert Grenadier";
+		hiddenSelections[]=
+		{
+			"Camo1",
+			"Camo2",
+			"Camo3"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"53rd_SC_aux\Armor\Hephaestus_Camo\Rifleman\53rd_Desert_Camo_Top_co.paa",
+			"53rd_SC_aux\Armor\Hephaestus_Camo\Rifleman\53rd_Camo_Pants_Desert_co.paa",
+			"53rd_SC_aux\Armor\Hephaestus_Camo\Grenadier\53rd_Grenadier_Armor_Desert_co.paa"
+		};
+		model="\Halo_marine_02\Halo_Marine_02_Uniform_F.p3d";
+		uniformClass="53rd_Uniform_GL_DS";
+	};
+
+};
